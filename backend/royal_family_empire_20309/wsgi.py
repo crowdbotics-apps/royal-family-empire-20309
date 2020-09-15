b320834eb43c076dc301186010296acfa08694d2
Crowdbotics <team@crowdbotics.com>
2020-09-15 21:19:49 +0000

    Update models

--- backend/royal_family_empire_20309/wsgi.py
+++ backend/royal_family_empire_20309/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'royal_family_empire_20309.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "royal_family_empire_20309.settings")
 
 application = get_wsgi_application()
